ead3712861b8839ee10370a340c22533f1e29ba0
Dablakbandit <ashle.thew@gmail.com>
2021-03-24 23:49:29 +1300

    Fix lint errors

--- src/App.js
+++ src/App.js
@@ -13,15 +13,16 @@ class App extends Component {
       count: prevState.count + amount,
     }));
 
-  // eslint-disable-next-line react/sort-comp
-  increment = this.makeIncrementer(1);
-
   render() {
     const { count } = this.state;
     return (
       <div>
         <p>Count: {count}</p>
-        <button type="button" className="increment" onClick={this.increment}>
+        <button
+          type="button"
+          className="increment"
+          onClick={this.makeIncrementer(1)}
+        >
           Increment count
         </button>
       </div>
