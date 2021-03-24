687ab3d3dbdd6e66d7b2c75f0fa96ca519ef2d69
Dablakbandit <ashle.thew@gmail.com>
2021-03-24 23:36:12 +1300

    Fix eslint problems

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
+  increment = this.makeIncrementer(1);
+
   constructor() {
     super();
     this.state = {
       count: 0,
-    }
+    };
   }
 
-  makeIncrementer = amount => () =>
-  this.setState(prevState => ({ 
-    count: prevState.count + amount,
-   }));
-   
-   increment = this.makeIncrementer(1);
-   
-   render() {
-    return (<div>
-      <p>Count: {this.state.count}</p>
-      <button className="increment" onClick={this.increment}>Increment count</button>
+  makeIncrementer = (amount) => () =>
+    this.setState((prevState) => ({
+      count: prevState.count + amount,
+    }));
+
+  render() {
+    const { items } = this.state;
+    return (
+      <div>
+        <p>Count: {items.count}</p>
+        <button type="button" className="increment" onClick={this.increment}>
+          Increment count
+        </button>
       </div>
-      )
+    );
   }
-} export default App;
+}
+export default App;
